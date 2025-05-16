4e007d0366d7a1dbbbe1a2113446a3ad3f91f9ae
Maite <martinezf.maite@gmail.com>
2025-05-15 22:12:44 -0300

    Creates the first model

--- ServiPuntosUy/DAO/Data/Central/CentralDbContext.cs
+++ ServiPuntosUy/DAO/Data/Central/CentralDbContext.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using ServiPuntosUy.DAO.Models.Central;
 
 namespace ServiPuntosUy.DAO.Data.Central;
 
 public class CentralDbContext : DbContext
 {
     public CentralDbContext(DbContextOptions<CentralDbContext> options) : base(options) {}
-    // public DbSet<User> Users { get; set; }
+
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
         base.OnModelCreating(modelBuilder);
--- ServiPuntosUy/Program.cs
+++ ServiPuntosUy/Program.cs
@@ -30,5 +30,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-
 app.Run();
