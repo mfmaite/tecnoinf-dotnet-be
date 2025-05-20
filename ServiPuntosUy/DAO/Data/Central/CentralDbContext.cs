2bbd4c337a21a282104be27ecf3afdb1a9233eb6
fvigliarolo <fd.vigliarolo@gmail.com>
2025-05-20 17:17:43 -0300

    login utilizando BD

--- ServiPuntosUy/DAO/Data/Central/CentralDbContext.cs
+++ ServiPuntosUy/DAO/Data/Central/CentralDbContext.cs
@@ -7,10 +7,29 @@ public class CentralDbContext : DbContext
 {
     public CentralDbContext(DbContextOptions<CentralDbContext> options) : base(options) {}
 
-    public DbSet<User> Users { get; set; }
+    public DbSet<CentralUser> Users { get; set; }
     public DbSet<Tenant> Tenants { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
         base.OnModelCreating(modelBuilder);
         // Aquí irán las configuraciones de las entidades
+                // Inserta datos iniciales para la tabla Users
+        modelBuilder.Entity<CentralUser>().HasData(
+            new CentralUser
+            {
+                Id = 1,
+                Name = "Admin",
+                Email = "admin@example.com",
+                Password = "hashed_password", // Asegúrate de usar un hash real
+            },
+            new CentralUser
+            {
+                Id = 2,
+                Name = "User",
+                Email = "user@example.com",
+                Password = "hashed_password", // Asegúrate de usar un hash real
+            }
+        );
+
     }
+
 }
