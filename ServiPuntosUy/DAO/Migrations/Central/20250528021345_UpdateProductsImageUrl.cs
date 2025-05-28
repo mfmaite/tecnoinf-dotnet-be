3156d838c2630410e9d5e1bba462f4cfdb92ab44
Maite <martinezf.maite@gmail.com>
2025-05-27 23:14:24 -0300

    Add migration

---
+++ ServiPuntosUy/DAO/Migrations/Central/20250528021345_UpdateProductsImageUrl.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiPuntosUy.DAO.Migrations.Central
+{
+    /// <inheritdoc />
+    public partial class UpdateProductsImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImgaeUrl",
+                table: "Products",
+                newName: "ImageUrl");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Password", "PasswordSalt" },
+                values: new object[] { "xZTYp+1wW9S0zRKK5Zma72x6rdOnUwvKucGrx5ewYUs=", "uqVv29SziAuBEu2U5VDSow==" });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Password", "PasswordSalt" },
+                values: new object[] { "xZTYp+1wW9S0zRKK5Zma72x6rdOnUwvKucGrx5ewYUs=", "uqVv29SziAuBEu2U5VDSow==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Products",
+                newName: "ImgaeUrl");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Password", "PasswordSalt" },
+                values: new object[] { "k/NFv1d2cH/NRAb9EhsQprd/sL9e8Ey44kh34zPSlRw=", "TQLRTQLr6vKr07vrPZk+RQ==" });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Password", "PasswordSalt" },
+                values: new object[] { "k/NFv1d2cH/NRAb9EhsQprd/sL9e8Ey44kh34zPSlRw=", "TQLRTQLr6vKr07vrPZk+RQ==" });
+        }
+    }
+}
